b7963b3ec99760a0c7b7e471905caed3db6ad096
root <root@localhost.localdomain>
2014-10-21 14:15:10 -0700

    Changed strategy seeds from JSON to YAML.

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-require 'json'
+require 'yaml'
 
 GameSource.create(:name => '81Dojo', :pass => 'shogi81', :kifu_url_header => 'http://81dojo.com/kifuviewer_jp.html?kid=', :category => 2)
 Handicap.create(:id => 1, :name => 'Even')
@@ -19,9 +19,7 @@ Handicap.create(:id => 7, :name => '4-piece')
 Handicap.create(:id => 8, :name => '6-piece')
 Handicap.create(:id => 9, :name => '8-piece')
 
-strategy_file_path = './db/strategy_seeds.json'
-file=File.read(strategy_file_path)
-js_data = JSON.parse(file)
+js_data = YAML.load_file("./db/strategy_seeds.yml")
 
 js_data["handicaps"].each do |handicap|
   board0 = ApplicationHelper::Board.new
