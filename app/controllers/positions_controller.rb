7766b633df0f08c31662776ad473e834a9200423
Hidetchi <hidetchi@81dojo.com>
2014-10-30 02:08:12 -0700

    Added test spec/controller/positions_controller_spec.rb, and some bugfix.

--- app/controllers/positions_controller.rb
+++ app/controllers/positions_controller.rb
@@ -7,11 +7,14 @@ class PositionsController < ApplicationController
   end
 
   def show
+    board = Board.new
     if (params[:sfen])
-      @position = Position.find_by(sfen: params[:sfen])
+      board.set_from_sfen(params[:sfen])
+      @position = Position.find_by(sfen: board.to_sfen)
     elsif (params[:sfen1])
       sfens = [params[:sfen1], params[:sfen2], params[:sfen3], params[:sfen4], params[:sfen5], params[:sfen6], params[:sfen7], params[:sfen8], params[:sfen9]]
-      @position = Position.find_by(sfen: sfens.join("/"))
+      board.set_from_sfen(sfens.join("/"))
+      @position = Position.find_by(sfen: board.to_sfen)
     else
       @position = Position.find_by(id: params[:id])
     end
