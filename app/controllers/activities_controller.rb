f32452d3f2a113417f5164499967531a5e990c16
Hidetchi <hidetchi@81dojo.com>
2014-11-25 05:30:25 +0900

    Resolved N+1 of activities#index

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -4,8 +4,8 @@ class ActivitiesController < ApplicationController
       redirect_to "/positions/start"
       return
     end
-    activities_user = Activity.where('owner_id = ? OR (recipient_type = ? AND recipient_id = ?)', current_user.id, 'User', current_user.id).order('created_at desc').limit(50)
-    activities_position = Activity.joins(:watchers).where('user_id = ?', current_user.id).order('created_at desc').limit(50)
+    activities_user = Activity.includes(:owner).includes(:recipient).includes(:trackable).where('owner_id = ? OR (recipient_type = ? AND recipient_id = ?)', current_user.id, 'User', current_user.id).order('created_at desc').limit(50)
+    activities_position = Activity.includes(:owner).includes(:recipient).includes(:trackable).joins(:watchers).where('user_id = ?', current_user.id).order('created_at desc').limit(50)
     @activities = activities_user | activities_position
     @activities.sort_by!{|item| item.created_at}.reverse!
     @positions_count = Position.count
