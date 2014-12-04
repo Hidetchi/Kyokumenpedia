b8a02ab3da897bf973594499a6a399f4bb884410
Hidetchi <hidetchi@81dojo.com>
2014-12-04 10:55:22 +0900

    Modified activity timeline

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,4 +1,6 @@
 class Activity < PublicActivity::Activity
   belongs_to :position, -> { where(activities: {recipient_type: 'Position'}) }, foreign_key: 'recipient_id'
   has_many :watchers, through: :position, source: :watchers
+  belongs_to :actor, -> { where(activities: {owner_type: 'User'}) }, class_name: 'User', foreign_key: 'owner_id'
+  has_many :followers, through: :actor, source: :followers
 end
