fb3409325d48214f8febb6339fdf22bacc09aebb
Hidetchi <hidetchi@81dojo.com>
2014-12-29 06:55:21 +0900

    Use Mysql for development as well.

--- db/schema.rb
+++ db/schema.rb
@@ -26,9 +26,9 @@ ActiveRecord::Schema.define(version: 20141219205359) do
     t.datetime "updated_at"
   end
 
-  add_index "activities", ["owner_id", "owner_type"], name: "index_activities_on_owner_id_and_owner_type"
-  add_index "activities", ["recipient_id", "recipient_type"], name: "index_activities_on_recipient_id_and_recipient_type"
-  add_index "activities", ["trackable_id", "trackable_type"], name: "index_activities_on_trackable_id_and_trackable_type"
+  add_index "activities", ["owner_id", "owner_type"], name: "index_activities_on_owner_id_and_owner_type", using: :btree
+  add_index "activities", ["recipient_id", "recipient_type"], name: "index_activities_on_recipient_id_and_recipient_type", using: :btree
+  add_index "activities", ["trackable_id", "trackable_type"], name: "index_activities_on_trackable_id_and_trackable_type", using: :btree
 
   create_table "appearances", force: true do |t|
     t.integer "game_id"
@@ -37,8 +37,8 @@ ActiveRecord::Schema.define(version: 20141219205359) do
     t.integer "next_move_id"
   end
 
-  add_index "appearances", ["game_id"], name: "index_appearances_on_game_id"
-  add_index "appearances", ["position_id"], name: "index_appearances_on_position_id"
+  add_index "appearances", ["game_id"], name: "index_appearances_on_game_id", using: :btree
+  add_index "appearances", ["position_id"], name: "index_appearances_on_position_id", using: :btree
 
   create_table "books", force: true do |t|
     t.string   "isbn13"
@@ -50,7 +50,7 @@ ActiveRecord::Schema.define(version: 20141219205359) do
     t.datetime "updated_at"
   end
 
-  add_index "books", ["isbn13"], name: "index_books_on_isbn13", unique: true
+  add_index "books", ["isbn13"], name: "index_books_on_isbn13", unique: true, using: :btree
 
   create_table "delayed_jobs", force: true do |t|
     t.integer  "priority",   default: 0, null: false
@@ -66,7 +66,7 @@ ActiveRecord::Schema.define(version: 20141219205359) do
     t.datetime "updated_at"
   end
 
-  add_index "delayed_jobs", ["priority", "run_at"], name: "delayed_jobs_priority"
+  add_index "delayed_jobs", ["priority", "run_at"], name: "delayed_jobs_priority", using: :btree
 
   create_table "discussions", force: true do |t|
     t.integer  "position_id"
@@ -77,8 +77,8 @@ ActiveRecord::Schema.define(version: 20141219205359) do
     t.datetime "updated_at"
   end
 
-  add_index "discussions", ["created_at"], name: "index_discussions_on_created_at"
-  add_index "discussions", ["position_id"], name: "index_discussions_on_position_id"
+  add_index "discussions", ["created_at"], name: "index_discussions_on_created_at", using: :btree
+  add_index "discussions", ["position_id"], name: "index_discussions_on_position_id", using: :btree
 
   create_table "editor_pickups", force: true do |t|
     t.integer  "position_id"
@@ -96,7 +96,7 @@ ActiveRecord::Schema.define(version: 20141219205359) do
     t.datetime "updated_at"
   end
 
-  add_index "follows", ["follower_id", "followed_id"], name: "index_follows_on_follower_id_and_followed_id", unique: true
+  add_index "follows", ["follower_id", "followed_id"], name: "index_follows_on_follower_id_and_followed_id", unique: true, using: :btree
 
   create_table "game_sources", force: true do |t|
     t.string   "name"
@@ -126,7 +126,7 @@ ActiveRecord::Schema.define(version: 20141219205359) do
     t.string   "event"
   end
 
-  add_index "games", ["csa_hash"], name: "index_games_on_csa_hash", unique: true
+  add_index "games", ["csa_hash"], name: "index_games_on_csa_hash", unique: true, using: :btree
 
   create_table "handicaps", force: true do |t|
     t.string   "name"
@@ -141,7 +141,7 @@ ActiveRecord::Schema.define(version: 20141219205359) do
     t.datetime "updated_at"
   end
 
-  add_index "headlines", ["name"], name: "index_headlines_on_name", unique: true
+  add_index "headlines", ["name"], name: "index_headlines_on_name", unique: true, using: :btree
 
   create_table "moves", force: true do |t|
     t.integer  "prev_position_id"
@@ -157,9 +157,9 @@ ActiveRecord::Schema.define(version: 20141219205359) do
     t.integer  "stat3_total",      default: 0
   end
 
-  add_index "moves", ["next_position_id"], name: "index_moves_on_next_position_id"
-  add_index "moves", ["prev_position_id", "next_position_id"], name: "index_moves_on_prev_position_id_and_next_position_id", unique: true
-  add_index "moves", ["prev_position_id"], name: "index_moves_on_prev_position_id"
+  add_index "moves", ["next_position_id"], name: "index_moves_on_next_position_id", using: :btree
+  add_index "moves", ["prev_position_id", "next_position_id"], name: "index_moves_on_prev_position_id_and_next_position_id", unique: true, using: :btree
+  add_index "moves", ["prev_position_id"], name: "index_moves_on_prev_position_id", using: :btree
 
   create_table "positions", force: true do |t|
     t.string   "sfen"
@@ -180,8 +180,8 @@ ActiveRecord::Schema.define(version: 20141219205359) do
     t.integer  "stat3_draw",     default: 0
   end
 
-  add_index "positions", ["sfen"], name: "index_positions_on_sfen", unique: true
-  add_index "positions", ["views"], name: "index_positions_on_views"
+  add_index "positions", ["sfen"], name: "index_positions_on_sfen", unique: true, using: :btree
+  add_index "positions", ["views"], name: "index_positions_on_views", using: :btree
 
   create_table "rs_evaluations", force: true do |t|
     t.string   "reputation_name"
@@ -189,45 +189,45 @@ ActiveRecord::Schema.define(version: 20141219205359) do
     t.string   "source_type"
     t.integer  "target_id"
     t.string   "target_type"
-    t.float    "value",           default: 0.0
+    t.float    "value",           limit: 24, default: 0.0
     t.datetime "created_at"
     t.datetime "updated_at"
     t.text     "data"
   end
 
-  add_index "rs_evaluations", ["reputation_name", "source_id", "source_type", "target_id", "target_type"], name: "index_rs_evaluations_on_reputation_name_and_source_and_target", unique: true
-  add_index "rs_evaluations", ["reputation_name"], name: "index_rs_evaluations_on_reputation_name"
-  add_index "rs_evaluations", ["source_id", "source_type"], name: "index_rs_evaluations_on_source_id_and_source_type"
-  add_index "rs_evaluations", ["target_id", "target_type"], name: "index_rs_evaluations_on_target_id_and_target_type"
+  add_index "rs_evaluations", ["reputation_name", "source_id", "source_type", "target_id", "target_type"], name: "index_rs_evaluations_on_reputation_name_and_source_and_target", unique: true, using: :btree
+  add_index "rs_evaluations", ["reputation_name"], name: "index_rs_evaluations_on_reputation_name", using: :btree
+  add_index "rs_evaluations", ["source_id", "source_type"], name: "index_rs_evaluations_on_source_id_and_source_type", using: :btree
+  add_index "rs_evaluations", ["target_id", "target_type"], name: "index_rs_evaluations_on_target_id_and_target_type", using: :btree
 
   create_table "rs_reputation_messages", force: true do |t|
     t.integer  "sender_id"
     t.string   "sender_type"
     t.integer  "receiver_id"
-    t.float    "weight",      default: 1.0
+    t.float    "weight",      limit: 24, default: 1.0
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  add_index "rs_reputation_messages", ["receiver_id", "sender_id", "sender_type"], name: "index_rs_reputation_messages_on_receiver_id_and_sender", unique: true
-  add_index "rs_reputation_messages", ["receiver_id"], name: "index_rs_reputation_messages_on_receiver_id"
-  add_index "rs_reputation_messages", ["sender_id", "sender_type"], name: "index_rs_reputation_messages_on_sender_id_and_sender_type"
+  add_index "rs_reputation_messages", ["receiver_id", "sender_id", "sender_type"], name: "index_rs_reputation_messages_on_receiver_id_and_sender", unique: true, using: :btree
+  add_index "rs_reputation_messages", ["receiver_id"], name: "index_rs_reputation_messages_on_receiver_id", using: :btree
+  add_index "rs_reputation_messages", ["sender_id", "sender_type"], name: "index_rs_reputation_messages_on_sender_id_and_sender_type", using: :btree
 
   create_table "rs_reputations", force: true do |t|
     t.string   "reputation_name"
-    t.float    "value",           default: 0.0
+    t.float    "value",           limit: 24, default: 0.0
     t.string   "aggregated_by"
     t.integer  "target_id"
     t.string   "target_type"
-    t.boolean  "active",          default: true
+    t.boolean  "active",                     default: true
     t.datetime "created_at"
     t.datetime "updated_at"
     t.text     "data"
   end
 
-  add_index "rs_reputations", ["reputation_name", "target_id", "target_type"], name: "index_rs_reputations_on_reputation_name_and_target", unique: true
-  add_index "rs_reputations", ["reputation_name"], name: "index_rs_reputations_on_reputation_name"
-  add_index "rs_reputations", ["target_id", "target_type"], name: "index_rs_reputations_on_target_id_and_target_type"
+  add_index "rs_reputations", ["reputation_name", "target_id", "target_type"], name: "index_rs_reputations_on_reputation_name_and_target", unique: true, using: :btree
+  add_index "rs_reputations", ["reputation_name"], name: "index_rs_reputations_on_reputation_name", using: :btree
+  add_index "rs_reputations", ["target_id", "target_type"], name: "index_rs_reputations_on_target_id_and_target_type", using: :btree
 
   create_table "simple_captcha_data", force: true do |t|
     t.string   "key",        limit: 40
@@ -236,7 +236,7 @@ ActiveRecord::Schema.define(version: 20141219205359) do
     t.datetime "updated_at"
   end
 
-  add_index "simple_captcha_data", ["key"], name: "idx_key"
+  add_index "simple_captcha_data", ["key"], name: "idx_key", using: :btree
 
   create_table "strategies", force: true do |t|
     t.string   "name"
@@ -246,7 +246,7 @@ ActiveRecord::Schema.define(version: 20141219205359) do
     t.integer  "main_position_id"
   end
 
-  add_index "strategies", ["ancestry"], name: "index_strategies_on_ancestry"
+  add_index "strategies", ["ancestry"], name: "index_strategies_on_ancestry", using: :btree
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "",   null: false
@@ -277,11 +277,11 @@ ActiveRecord::Schema.define(version: 20141219205359) do
     t.integer  "role",                   default: 0
   end
 
-  add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["point"], name: "index_users_on_point"
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-  add_index "users", ["username"], name: "index_users_on_username", unique: true
+  add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["point"], name: "index_users_on_point", using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+  add_index "users", ["username"], name: "index_users_on_username", unique: true, using: :btree
 
   create_table "watches", force: true do |t|
     t.integer  "user_id"
@@ -291,9 +291,9 @@ ActiveRecord::Schema.define(version: 20141219205359) do
     t.datetime "updated_at"
   end
 
-  add_index "watches", ["position_id"], name: "index_watches_on_position_id"
-  add_index "watches", ["user_id", "position_id"], name: "index_watches_on_user_id_and_position_id", unique: true
-  add_index "watches", ["user_id"], name: "index_watches_on_user_id"
+  add_index "watches", ["position_id"], name: "index_watches_on_position_id", using: :btree
+  add_index "watches", ["user_id", "position_id"], name: "index_watches_on_user_id_and_position_id", unique: true, using: :btree
+  add_index "watches", ["user_id"], name: "index_watches_on_user_id", using: :btree
 
   create_table "wikiposts", force: true do |t|
     t.integer  "position_id"
@@ -309,8 +309,8 @@ ActiveRecord::Schema.define(version: 20141219205359) do
     t.integer  "likes",        default: 0
   end
 
-  add_index "wikiposts", ["created_at"], name: "index_wikiposts_on_created_at"
-  add_index "wikiposts", ["position_id"], name: "index_wikiposts_on_position_id"
-  add_index "wikiposts", ["user_id"], name: "index_wikiposts_on_user_id"
+  add_index "wikiposts", ["created_at"], name: "index_wikiposts_on_created_at", using: :btree
+  add_index "wikiposts", ["position_id"], name: "index_wikiposts_on_position_id", using: :btree
+  add_index "wikiposts", ["user_id"], name: "index_wikiposts_on_user_id", using: :btree
 
 end
