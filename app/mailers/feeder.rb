7f837015eb21befcf0538c784e6d1f05fb8868c8
Hidetchi <hidetchi@81dojo.com>
2015-01-06 01:41:37 +0900

    Changed mail templates.

--- app/mailers/feeder.rb
+++ app/mailers/feeder.rb
@@ -1,5 +1,5 @@
 class Feeder < ActionMailer::Base
-  default from: "kyokumenpedia@iscube.com"
+  default from: "info@kyokumen.jp"
   SUBJECT_HEADER = "【" + SITE_NAME + "】"
   
   def wikipost_to_watcher(recipient_id, wikipost_id)
