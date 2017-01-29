060c4acac4459117d5f1eef3fd6ea0193ecfdb5a
Hidetchi <hidetchi@81dojo.com>
2017-01-29 21:01:44 +0900

    Changed column name.

--- db/migrate/20170129030904_create_shogidb2_entries.rb
+++ db/migrate/20170129030904_create_shogidb2_entries.rb
@@ -1,10 +1,10 @@
 class CreateShogidb2Entries < ActiveRecord::Migration
   def change
     create_table :shogidb2_entries do |t|
-      t.string :hash
+      t.string :native_hash
       t.integer :result
 
-      t.index :hash, unique: true
+      t.index :native_hash, unique: true
     end
   end
 end
